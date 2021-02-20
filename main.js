2b12d1934e60f3b33cc1eced6189191d893f6816
fabacab <fabacab@riseup.net>
2021-02-20 14:52:46 -0500

    Better view.

--- main.js
+++ main.js
@@ -1,23 +1,35 @@
 (function () { // begin Immediately-Invoked Function Expression
     document.addEventListener('DOMContentLoaded', function() {
         var calendar = new FullCalendar.Calendar(document.getElementById('calendar'), {
+            headerToolbar: {
+                left: 'prev,next today',
+                center: 'title',
+                right: 'dayGridMonth,timeGridWeek,timeGridDay'
+            },
             eventSources: [
                 {
-                    url: 'https://techlearningcollective.com/events/all.ics',
-                    format: 'ics',
-                    title: 'Tech Learning Collective'
+                    // Use the FullCalendar custom JSON feed until its bug #6173 is resolved.
+                    // See https://github.com/fullcalendar/fullcalendar/issues/6173
+                    url: 'https://techlearningcollective.com/events/all-fullcalendar-io.json'
                 },
                 {
                     url: 'https://tockify.com/api/feeds/ics/mlsupport',
                     format: 'ics',
-                    title: 'MACC events'
-                },
-                {
-                    url: 'https://bluestockings.com/?plugin=all-in-one-event-calendar&controller=ai1ec_exporter_controller&action=export_events&no_html=true',
-                    format: 'ics',
-                    title: 'Bluestockings'
                 }
-            ]
+
+                // Bluestockings has not correctly configured CORS headers.
+//                {
+//                    url: 'https://bluestockings.com/?plugin=all-in-one-event-calendar&controller=ai1ec_exporter_controller&action=export_events&no_html=true',
+//                    format: 'ics',
+//                    title: 'Bluestockings'
+//                }
+            ],
+            eventClick: function (info) {
+                info.jsEvent.preventDefault();
+                if (info.event.url) {
+                    window.open(info.event.url);
+                }
+            }
         });
         calendar.render();
     });
