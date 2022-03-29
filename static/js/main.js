716bd7c0f7628642fd84abe9348b47b98845d161
fabacab <fabacab@riseup.net>
2022-03-28 22:53:18 -0400

    Add a little "featured events" section to signal boost, y'know.

--- static/js/main.js
+++ static/js/main.js
@@ -1,7 +1,20 @@
 import { default as calendar } from './calendar.js';
 
+function hidePastFeaturedEvents () {
+    var els = document.getElementById('featured-events').querySelectorAll('[data-not-valid-after]');
+    els.forEach(function (el) {
+        var date = new Date(el.dataset.notValidAfter);
+        var time = new Date();
+        if ( time >= date ) {
+            el.style.display = 'none';
+        }
+    });
+}
+
 if ('loading' === document.readyState) {
     document.addEventListener('DOMContentLoaded', calendar.render);
+    document.addEventListener('DOMContentLoaded', hidePastFeaturedEvents);
 } else {
     calendar.render();
+    hidePastFeaturedEvents();
 }
