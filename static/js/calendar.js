650c373f89afbcc3113125b6a745cc787c5b9c8a
fabacab <fabacab@riseup.net>
2022-04-19 11:09:33 -0400

    Add one off event source for Bluestockings.

--- static/js/calendar.js
+++ static/js/calendar.js
@@ -101,6 +101,30 @@ export default new FullCalendar.Calendar(document.getElementById('calendar'), {
             },
             color: 'purple'
         },
+
+        // This is actually Bluestockings, but they use Bookmanager for events now.
+        // So here is a simple implementation of a Bookmanager event scraper.
+        {
+            name: 'Bluestockings Cooperative',
+            id: 'bluestocksings-cooperative',
+            className: 'bluestockings-cooperative',
+            events: async function (fetchInfo, successCallback, failureCallback) {
+                var session_id = 'x'; // This can be whatever.
+                var store_id = '576827';
+                var response = await fetch('https://api.bookmanager.com/customer/event/getList?session_id=' + session_id + '&store_id=' + store_id);
+                var json = await response.json();
+                // This needs some work.
+                successCallback(json.rows.map(function (item) {
+                    var tz_offset_milliseconds = (new Date()).getTimezoneOffset() * 60 * 1000;
+                    return {
+                        title: item.info.name,
+                        start: new Date(item.from * 1000 - tz_offset_milliseconds),
+                        end: new Date(item.to * 1000 - tz_offset_milliseconds),
+                        url: 'https://bluestockings.com/events/' + item.id
+                    };
+                }));
+            }
+        }
     ]),
     eventDidMount: function (info) {
         info.el.setAttribute('title', info.event.title);
