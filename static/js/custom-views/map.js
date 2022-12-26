68e320d850fb4d7fca89353316f4ad49ad3be239
fabacab <fabacab@riseup.net>
2022-12-26 03:30:43 -0500

    Initial map view now loads with a Leaflet and OpenStreetMap map.

--- static/js/custom-views/map.js
+++ static/js/custom-views/map.js
@@ -3,11 +3,38 @@ import { sliceEvents, createPlugin } from '@fullcalendar/core';
 const MapViewConfig = {
     classNames: [ 'map-view' ],
     content: function (props) {
-        var segs = sliceEvents(props, true); // allDay=true
-        var html = 'this is a test';
+        var html = '<div id="map" style="height: 300px;"></div>';
         return {
             html: html
         }
+    },
+    didMount: function (props) {
+        // Center the map on New York City.
+        var map = L.map('map').setView([40.6975, -73.9795], 10);
+        L.tileLayer('https://tile.openstreetmap.org/{z}/{x}/{y}.png', {
+            maxZoom: 19,
+            attribution: '&copy; <a href="http://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors'
+        }).addTo(map);
+
+        // Get today's events.
+        var segs = sliceEvents(props, true); // allDay=true
+
+        // For each event, create a marker.
+        // TODO: Use GeoJSON layers per event source instead of event?
+        // TODO: Use GeoJSON features instead of simpler Points.
+        segs.forEach(function (e) {
+            if (e?.def?.extendedProps?.location?.geoJSON) {
+                console.log(e.def.extendedProps.location.geoJSON);
+                L.geoJSON(
+                    e.def.extendedProps.location.geoJSON
+                ).bindPopup(function (layer) {
+                    return e.def.extendedProps.description;
+                }).addTo(map);
+            }
+        });
+    },
+    willUnmount: function (props) {
+        // Code that will execute when the view is unloaded.
     }
 };
 
