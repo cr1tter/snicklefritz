5d41376b3efe165f3c96acec5aae88b7f469b784
fabacab <fabacab@riseup.net>
2022-12-26 03:51:59 -0500

    Use a GeoJSON FeatureCollection for ease of adding markers to map.

--- static/js/custom-views/map.js
+++ static/js/custom-views/map.js
@@ -9,29 +9,39 @@ const MapViewConfig = {
         }
     },
     didMount: function (props) {
+        // Get today's events.
+        var segs = sliceEvents(props, true); // allDay=true
+        var geojson = {
+            type: 'FeatureCollection',
+            features: []
+        };
+
+        segs.forEach(function (e) {
+            if (e.def.extendedProps?.location?.geoJSON) {
+                geojson.features.push({
+                    type: 'Feature',
+                    geometry: e.def.extendedProps.location.geoJSON,
+                    properties: {
+                        title: e.def.title,
+                        description: e.def.extendedProps?.description
+                    }
+                });
+            }
+        });
+
         // Center the map on New York City.
         var map = L.map('map').setView([40.6975, -73.9795], 10);
         L.tileLayer('https://tile.openstreetmap.org/{z}/{x}/{y}.png', {
             maxZoom: 19,
-            attribution: '&copy; <a href="http://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors'
+            attribution: '&copy; <a href="http://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors - See a mistake? <a href="https://openstreetmap.org/fixthemap">Fix the map</a>!'
         }).addTo(map);
 
-        // Get today's events.
-        var segs = sliceEvents(props, true); // allDay=true
-
-        // For each event, create a marker.
-        // TODO: Use GeoJSON layers per event source instead of event?
-        // TODO: Use GeoJSON features instead of simpler Points.
-        segs.forEach(function (e) {
-            if (e?.def?.extendedProps?.location?.geoJSON) {
-                console.log(e.def.extendedProps.location.geoJSON);
-                L.geoJSON(
-                    e.def.extendedProps.location.geoJSON
-                ).bindPopup(function (layer) {
-                    return e.def.extendedProps.description;
-                }).addTo(map);
+        // Add the features.
+        L.geoJSON(geojson, {
+            onEachFeature: function (feature, layer) {
+                layer.bindPopup(feature.properties.title + ' - ' + feature.properties.description);
             }
-        });
+        }).addTo(map);
     },
     willUnmount: function (props) {
         // Code that will execute when the view is unloaded.
