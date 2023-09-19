1a7c69f82672b037d9e61cb1226436160790df79
fabacab <fabacab@riseup.net>
2023-09-19 06:17:24 -0400

    Update FullCalendar to version 6.1.8.

--- static/js/custom-views/map.js
+++ static/js/custom-views/map.js
@@ -1,5 +1,5 @@
-import { sliceEvents, createPlugin } from 'https://cdn.skypack.dev/@fullcalendar/core@6.0.1?min';
-import momentTimezone from 'https://cdn.skypack.dev/moment-timezone@0.5.40?min';
+import { sliceEvents, createPlugin } from 'https://cdn.skypack.dev/@fullcalendar/core@6.1.8?min';
+import momentTimezone from 'https://cdn.skypack.dev/moment-timezone@0.5.43?min';
 import { default as calendar } from '../calendar.js';
 import FullCalendarEvent from '../event.js';
 
